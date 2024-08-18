00a466f309adc0aa98a98bf56b045300c96ba72b
Srinu-7 <srinumaka9908@gmail.com>
2024-08-18 21:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase to handle case insensitivity
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+  
+  // Find the index of the first occurrence of the lowercase s2 in lowercase s1
+  const index = lowerS1.indexOf(lowerS2);
+  
+  return index;
 }
 
 // Please do not change the code below
